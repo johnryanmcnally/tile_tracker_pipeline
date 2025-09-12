31070eac28e07ad38cd2a13809618bbb96b57584
johnryanmcnally <john.ryan.mcnally@gmail.com>
2025-09-12 16:58:01 -0500

    sqlite3 issue

--- data_dashboard/dashboard_utils.py
+++ data_dashboard/dashboard_utils.py
@@ -1,9 +1,9 @@
 # Native
 import os
 import datetime
-__import__('pysqlite3') 
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3') 
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sqlite3
 
 # Third Party
