017da1a1c621edfe968577b85392ce150bc16a51
johnryanmcnally <john.ryan.mcnally@gmail.com>
2025-10-21 11:05:33 -0300

    update langchain imports for new version

--- data_dashboard/dashboard_utils.py
+++ data_dashboard/dashboard_utils.py
@@ -18,8 +18,8 @@ from vega_datasets import data
 
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 
